0f8371c7c6ebf994fe36df957920da9610035077
nitap <mfaathinn@gmail.com>
2026-02-09 22:55:16 +0700

    week01: setup awal

---
+++ src/oop_00000130562_MuhammadFaathinNaufal/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000130562_MuhammadFaathinNaufal.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println("Nama: " + name + " , nilai " + score)
+}
