157e329179fab5e7a9e03a4ee3909c92785a7531
nitap <mfaathinn@gmail.com>
2026-02-12 14:01:33 +0700

    week01: (t1) ngerapihin

--- src/oop_00000130562_MuhammadFaathinNaufal/week01/CircularCalculator.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week01/CircularCalculator.kt
@@ -1,13 +1,13 @@
 package oop_00000130562_MuhammadFaathinNaufal.week01
 
-fun main(args: Array<String>) {
+fun main() {
     val radius = 7.0
     val pi = 3.14
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
-    println(checkSize(area)) // Panggil di dalam println
+    println(checkSize(area))
 }
 
 fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
-}
+
