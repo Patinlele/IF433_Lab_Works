c3d21a3ac1522944b4421a35859363dd4247a787
nitap <mfaathinn@gmail.com>
2026-02-12 14:24:42 +0700

    w1 t2pkai null safety for user note

--- src/oop_00000130562_MuhammadFaathinNaufal/week01/GameStore.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week01/GameStore.kt
@@ -4,13 +4,16 @@ fun main()
     val judul = "GTA VI"
     val price = 1200000
     val finalPrice = price - diskon(price)
-    printReceipt(judul , finalPrice = finalPrice)
+    val userNote: String? = null
+    printReceipt(judul , finalPrice = finalPrice, note = userNote)
 }
 fun diskon(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
 
-fun printReceipt(judul: String, finalPrice: Int) {
+fun printReceipt(judul: String, finalPrice: Int, note: String?) {
     println(">>>> Struk Pembelian <<<<<")
     println("Game: $judul")
     println("Harga Akhir: Rp $finalPrice")
+    println("note: ${note ?: "tak da notes"}")
+
 }
 
