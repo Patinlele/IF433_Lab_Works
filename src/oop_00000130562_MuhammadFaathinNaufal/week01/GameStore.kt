f8cf4ad278ef2160253e8c8330ef78043b38819b
nitap <mfaathinn@gmail.com>
2026-02-12 14:13:19 +0700

    w1 t2 print resi

--- src/oop_00000130562_MuhammadFaathinNaufal/week01/GameStore.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week01/GameStore.kt
@@ -3,8 +3,14 @@ fun main()
 {
     val judul = "GTA VI"
     val price = 1200000
-    val finalPricve = price - diskon(price)
+    val finalPrice = price - diskon(price)
+    printReceipt(judul , finalPrice = finalPrice)
 }
 fun diskon(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
-        ;
+
+fun printReceipt(judul: String, finalPrice: Int) {
+    println(">>>> Struk Pembelian <<<<<")
+    println("Game: $judul")
+    println("Harga Akhir: Rp $finalPrice")
+}
 
