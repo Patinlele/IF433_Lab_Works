90a546fe3ffc958552b5cf36c3a0523121edbdc6
nitap <mfaathinn@gmail.com>
2026-02-12 14:10:26 +0700

    w1 t2 logic diskon

--- src/oop_00000130562_MuhammadFaathinNaufal/week01/GameStore.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week01/GameStore.kt
@@ -3,5 +3,8 @@ fun main()
 {
     val judul = "GTA VI"
     val price = 1200000
+    val finalPricve = price - diskon(price)
 }
+fun diskon(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
+        ;
 
