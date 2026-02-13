3a3feba65d0532ab988009e6f457f09be65e83b6
nitap <mfaathinn@gmail.com>
2026-02-13 15:44:25 +0700

    W2: challenge refactor primary constructor using default args

--- src/oop_00000130562_MuhammadFaathinNaufal/week02/Student.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week02/Student.kt
@@ -3,7 +3,8 @@ package oop_00000130562_MuhammadFaathinNaufal.week02
 class Student (
     val name: String,
     val nim: String,
-    val major: String,
+    var major: String,
+    var gpa: Double = 0.0,
 
     ){
     init {
