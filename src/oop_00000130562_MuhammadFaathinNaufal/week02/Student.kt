ffc037dbe4933c1847f40ddc069db21238a21b1d
nitap <mfaathinn@gmail.com>
2026-02-13 13:24:55 +0700

    sedikit perubahan

--- src/oop_00000130562_MuhammadFaathinNaufal/week02/Student.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week02/Student.kt
@@ -1,11 +1,13 @@
 package oop_00000130562_MuhammadFaathinNaufal.week02
 
+package oop_00000130562_MuhammadFaathinNaufal.week02
+
 class Student (
     val name: String,
     val nim: String,
     val major: String,
 
-){
+    ){
     init {
         if (nim.length != 5){
             println("Warning: Objeck tercipta dengan NIM ($nim) yang tidak valid!")
