2af339bc0b085f552f22a540595f5b57fb85225d
nitap <mfaathinn@gmail.com>
2026-02-20 15:09:51 +0700

    W3: fix setter using backing field

--- src/oop_00000130562_MuhammadFaathinNaufal/week03/Employee.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week03/Employee.kt
@@ -3,9 +3,13 @@ package oop_00000130562_MuhammadFaathinNaufal.week03
 class Employee(val name: String){
     var salary: Int = 0
             set(value) {
-                println("Salary $value")
-                this.salary = value
-
-
+                if (value < 0){
+                    println("ERR: gaji ga boleh negatif!, Di set ke 0")
+                    field = 0
+                }
+                else{
+                    field = value
+                }
     }
 }
+
