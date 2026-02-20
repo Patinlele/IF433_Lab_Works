a414a69c076e7ab981d529728a43249008325b94
nitap <mfaathinn@gmail.com>
2026-02-20 15:20:00 +0700

    W3: test akk encapsulation logic in main

--- src/oop_00000130562_MuhammadFaathinNaufal/week03/Main.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week03/Main.kt
@@ -3,5 +3,12 @@ package oop_00000130562_MuhammadFaathinNaufal.week03
 fun main() {
     val e = Employee("Jokowi")
     println("ERR test")
-    e.salary = 5000
+    e.salary = 50000000
+    e.salary = -1000
+    println("GAji: ${e.salary}")
+
+    e.increasePerformance()
+
+    println("Pajak yang harus dibayar: ${e.tax}")
+
 }
