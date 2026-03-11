354deed59fe541dba181b880d6ee653805d106a7
nitap <mfaathinn@gmail.com>
2026-03-11 08:27:07 +0700

    W5 T2 bikin credit pinjol

---
+++ src/oop_00000130562_MuhammadFaathinNaufal/week05/CreditCard.kt
@@ -0,0 +1,19 @@
+package oop_00000130562_MuhammadFaathinNaufal.week05
+
+class CreditCard (accountName: String, val limit: Double) : PaymentMethod(accountName) {
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("[$accountName - Credit Card ] Pembayaran sukses. limit terpakai: $usedAmount/ $limit")
+
+
+        }else {
+            println(
+                "[$accountName - Credit Card] Transaksi ditolak. Melebihi limit."
+            )
+        }
+    }
+
+}
