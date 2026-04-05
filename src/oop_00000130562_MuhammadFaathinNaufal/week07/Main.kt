7530f6c9051192fdfbfd1ffc533629140e3a5432
nitap <mfaathinn@gmail.com>
2026-04-06 01:42:34 +0700

    W7 nyoba nyoba

--- src/oop_00000130562_MuhammadFaathinNaufal/week07/Main.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week07/Main.kt
@@ -40,4 +40,10 @@ fun main() {
     println("\n=== TEST GAME MANAGER SINGLETON ===")
     GameManager.startGame()
     GameManager.startGame()
+
+    println("\n=== TEST RARITY & FACTORY ===")
+    println("Drop chance LEGENDARY: ${ItemRarity.LEGENDARY.dropChance}%")
+
+    val starterWeapon = Weapon.forgeStarterSword()
+    println("Senjata awal: $starterWeapon")
 }
