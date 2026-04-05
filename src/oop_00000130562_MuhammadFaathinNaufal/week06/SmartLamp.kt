ed7a972cb890a7375cd6c6cf4f11a1e77eda8dbb
nitap <mfaathinn@gmail.com>
2026-04-06 02:09:47 +0700

    W7 bikin lampu

---
+++ src/oop_00000130562_MuhammadFaathinNaufal/week06/SmartLamp.kt
@@ -0,0 +1,4 @@
+class SmartLamp(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnon() { println("Lampu $name menyala.") }
+    override fun turnoff() { println("Lampu $name mati.") }
+}
