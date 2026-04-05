5d1c5983c4529ca6a6f278dc944fa82a5bc37721
nitap <mfaathinn@gmail.com>
2026-04-06 02:02:36 +0700

    W7 bikin camera & phone interface

---
+++ src/oop_00000130562_MuhammadFaathinNaufal/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000130562_MuhammadFaathinNaufal.week06
+
+interface Camera {
+    fun turnon() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnon() { println("Sinyal seluler mencari jaringan.") }
+}
