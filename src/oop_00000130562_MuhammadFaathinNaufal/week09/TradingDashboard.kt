bd8f0b45e8629306ec0110282e2999b8aef7726b
nitap <mfaathinn@gmail.com>
2026-04-27 02:04:38 +0700

    week09: (task) populate mock trade history list

--- src/oop_00000130562_MuhammadFaathinNaufal/week09/TradingDashboard.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week09/TradingDashboard.kt
@@ -1,4 +1,12 @@
 package oop_00000130562_MuhammadFaathinNaufal.week09
 
-class TradingDashboard {
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 50, 120.0, "OPEN"),
+        TradeLog("SOLUSDT", "LONG", 10, 8.5, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 20, -12.0, "CLOSED"),
+        TradeLog("BNBUSDT", "SHORT", 15, 0.0, "CLOSED")
+    )
 }
