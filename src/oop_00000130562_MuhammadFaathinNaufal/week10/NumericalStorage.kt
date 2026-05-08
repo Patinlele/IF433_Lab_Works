6f5ffe585daa0a3115fdf5021dc401b57d111f61
patin <mfaathinn@gmail.com>
2026-05-08 13:44:55 +0700

    W10: implement where clause for comparable constraints

--- src/oop_00000130562_MuhammadFaathinNaufal/week10/NumericalStorage.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week10/NumericalStorage.kt
@@ -6,6 +6,6 @@ class MathBox<T : Number>(val value: T, val value2: T) {
     }
 }
 
-fun <T> gertMax(a: T, b: T): T where T : Comaparable<T> {
+fun <T> gertMax(a: T, b: T): T where T : Comparable<T> {
     return if (a > b) a else b
 }
