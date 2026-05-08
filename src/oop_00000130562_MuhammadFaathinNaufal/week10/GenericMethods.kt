e7aa13b823e24be617ea73527064f10af93e790a
patin <mfaathinn@gmail.com>
2026-05-08 13:38:49 +0700

    W10: implement generic function with return type

--- src/oop_00000130562_MuhammadFaathinNaufal/week10/GenericMethods.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week10/GenericMethods.kt
@@ -4,3 +4,8 @@ package oop_00000130562_MuhammadFaathinNaufal.week10
 fun <T> printData(data: T){
     println("Data yang diterima: $data")
 }
+
+fun <T> processData(input: T): T {
+    return input
+}
+
