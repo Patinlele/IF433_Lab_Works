6085c4f746ad49bdb0411e81ed780e633e7456d6
nitap <mfaathinn@gmail.com>
2026-05-14 20:49:08 +0700

    week11: create string extension functions

---
+++ src/oop_00000130562_MuhammadFaathinNaufal/week11/StringExtension.kt
@@ -0,0 +1,9 @@
+package oop_00000130562_MuhammadFaathinNaufal.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
