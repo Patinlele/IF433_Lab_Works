22e613e3f6b7460f4ad4372a92389ff3178339c1
nitap <mfaathinn@gmail.com>
2026-06-07 14:45:26 +0700

    week12: (task) execute feeding using runCatching

--- src/oop_00000130562_MuhammadFaathinNaufal/week12/SmartFeeder.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week12/SmartFeeder.kt
@@ -30,4 +30,8 @@ fun main() {
     } finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+    println("\n=== JADWAL MAKAN 2: SORE ===")
+    val result = runCatching {
+        dispenseKibble(30, 1000, false)
+    }
 }
