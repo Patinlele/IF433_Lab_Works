6e4c566cb1262009ec291c74110901c7beb00802
nitap <mfaathinn@gmail.com>
2026-06-07 15:25:23 +0700

    week14: add SeniorDiscount without modifying calculator

--- src/oop_00000130562_MuhammadFaathinNaufal/week14/OcpRefactored.kt
+++ src/oop_00000130562_MuhammadFaathinNaufal/week14/OcpRefactored.kt
@@ -17,4 +17,9 @@ class SafeDiscountCalculator(private val strategy: DiscountStrategy) {
     fun calculate(price: Double) = strategy.apply(price)
 }
 
+class SeniorDiscount: DiscountStrategy {
+    override fun apply(price: Double) = price * 0.75
+}
+
+
 
